0e34d535941031c57a0fe687cb22c43cbc7367fe
daithi <daithi@daithi-laptop>
2013-02-07 17:06:29 +0000

    flowdock and cioauth2

---
+++ flowdock/index.php
@@ -0,0 +1,44 @@
+<?php
+/*
+  Plugin Name: Flowdock
+  Plugin URI: https://github.com/david-coombes/api-con-mngr-modules
+  Description: Flowdock login module for API Connection Manager
+  Version: 0.1
+  Author: Daithi Coombes
+  Author URI: http://david-coombes.com
+ */
+if(!class_exists("FlowDock_API")):
+	class FlowDock_API extends API_Con_Mngr_Module{
+
+		function __construct() {
+
+			$this->protocol = "custom";
+			$this->options['apikey'] = "%s";
+
+			parent::__construct();
+		}
+
+		function get_uid(){
+
+		}
+
+		function request( $url, $method='get', $parameters=array(), $die=true){
+			
+			if(!count($this->headers))
+				$this->headers['content-type'] = 'application/x-www-form-urlencoded';
+			
+			return parent::request($url, $method, $parameters, $die);
+
+		}
+		
+		function verify_token() {
+			;
+		}
+
+		function check_error(array $response) {
+			;
+		}
+	}
+endif;
+
+$module = new FlowDock_API();
