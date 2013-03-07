84608374b6c86cdd3e49ad93094cc349023f9c13
daithi <daithi@daithi-laptop>
2013-03-07 21:23:29 +0000

    update ciauth endpoint

--- ci-login/index.php
+++ ci-login/index.php
@@ -14,7 +14,8 @@ if(!class_exists("CityIndex_API")):
 	
 	class CityIndex_API extends API_Con_Mngr_Module{
 	
-		private $endpoint = "http://ec2-107-22-63-28.compute-1.amazonaws.com/ciauth";
+		//private $endpoint = "http://ec2-107-22-63-28.compute-1.amazonaws.com/ciauth";
+		private $endpoint = "http://ec2-23-21-217-245.compute-1.amazonaws.com/ciauth";
 	
 		function __construct(){
 			
@@ -52,73 +53,6 @@ if(!class_exists("CityIndex_API")):
 			return true;
 		}
 	}
-	
-	/**
-	 * @deprecated use the oauth2 class above
-	 *
-	class CityIndex_API extends API_Con_Mngr_Module{
-		
-		private $api;
-		private $username='';
-		private $endpoint;
-		protected $session='';
-	
-		function __construct(){
-			
-			require_once('CIAPI-PHP/CIAPIPHP.class.php');
-			$this->api = new CIAPIPHP();
-			$this->endpoint = "https://ciapipreprod.cityindextest9.co.uk/tradingapi/";
-			$this->login_form = array(
-				'fields' => array(
-					'text' => 'UserName',
-					'password' => 'Password'
-				),
-				'endpoint' => false
-			);
-			$this->protocol = "service";
-			
-			parent::__construct();
-		}
-	
-		function check_error( array $res ){
-			
-			if($this->api->get_errors())
-				print $this->get_login_button();
-				
-			return false;
-		}
-		
-		function get_uid(){
-			return false;
-		}
-		
-		function login_form_callback( stdClass $dto){
-			
-			//login
-			$res = $this->api->logIn( $dto->response['UserName'], $dto->response['Password'] );
-			$this->check_error( (array) $res );
-
-			return $res->Session;
-		}
-		
-		function request( $target, $method='get', $parameters=array(), $die=true){
-			
-			$url = $this->endpoint . trim($target,"/");
-			$this->headers = array(
-				'UserName' => "DM509022", //$this->username,
-				'Session' => $this->session
-			);
-			$this->log($this->headers);
-			
-			return parent::request($url, $method, $parameters, $die);
-		}
-		
-		function verify_token(){
-			return true;
-		}
-	}
-	 * 
-	 */
 endif;
 
 $module = new CityIndex_API();
