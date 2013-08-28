75087e3a94b862522f914a448b3e9f6c2eefa118
daithi-coombes <webeire@gmail.com>
2013-08-28 02:17:06 +0100

    new class

--- ci-login/index.php
+++ ci-login/index.php
@@ -14,9 +14,31 @@ if ( !class_exists( 'CIAPIPHP' ) )
 if(!class_exists("CityIndex_API")):
 	
 	class CityIndex_API extends API_Con_Mngr_Module{
-	
 		
-	
+		function __construct(){
+
+			$this->endpoint = "http://23.21.217.245/ciauth/authorize";
+			$this->protocol = "oauth2";
+
+			parent::__construct();
+		}
+
+		function check_error( array $response ){
+			return false;
+		}
+
+		function get_uid(){
+
+		}
+
+		function get_profile(){
+
+		}
+
+		function verify_token(){
+
+		}
+
 	}
 endif;
 
