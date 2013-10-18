7c26bad6d8c319c78e854b225bef17556a6b9a21
daithi-coombes <webeire@gmail.com>
2013-10-18 13:43:47 +0100

    test fb check for error

--- facebook/index.php
+++ facebook/index.php
@@ -42,7 +42,10 @@ class Facebook_API extends API_Con_Mngr_Module{
 	}
 
 	function check_error(array $response) {
-		var_dump($response);
+		$data = json_decode( $response['body'] );
+		var_dump($data);
+		if ( $data->error )
+			return new API_Con_Error( $data->error->message );
 	}
 
 	function request($url, $method='GET', $parameters = array(), $die=true){
